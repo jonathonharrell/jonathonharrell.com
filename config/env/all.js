c3515cee0a029bedf696088e55945e44f06595f5
Jonathon Harrell <jonathonh@etrak.com>
2014-04-17 13:50:34 -0500

    Initial commit

---
+++ config/env/all.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var path = require('path'),
+	rootPath = path.normalize(__dirname + '/../..');
+
+module.exports = {
+	app: {
+		title: 'Jonathon Harrell',
+		description: 'Web developer, node enthusiast.',
+		keywords: 'web, developer, designer, node, javascript'
+	},
+	root: rootPath,
+	port: process.env.PORT || 3000,
+	templateEngine: 'swig',
+	sessionSecret: 'jonathon-harrell',
+	sessionCollection: 'sessions'
+};
