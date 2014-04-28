7a9599a0089320d2dc6e8aac2d470210099a0025
Jonathon Harrell <jonathonh@etrak.com>
2014-04-28 11:59:09 -0500

    Worked on charts, finished about me section

--- public/modules/core/controllers/home.js
+++ public/modules/core/controllers/home.js
@@ -8,7 +8,10 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
             //This is the Main Highcharts chart config. Any Highchart options are valid here.
             //will be ovverriden by values specified below.
             chart: {
-                type: 'column'
+                type: 'column',
+                style: {
+                    backgroundColor: null
+                }
             },
             tooltip: {
                 style: {
@@ -16,6 +19,9 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
                     fontWeight: 'bold'
                 }
             },
+            exporting: {
+                enabled: false
+            }
         },
 
         //Series object (optional) - a list of series using normal highcharts series options.
@@ -55,7 +61,7 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
 
         //Title configuration (optional)
         title: {
-            text: 'Awesome Web Dev Chops'
+            text: null
         },
 
         //Boolean to control showng loading status on chart (optional)
@@ -66,13 +72,71 @@ angular.module('core').controller('HomeController', ['$scope', 'Authentication',
         xAxis: {
             currentMin: 0,
             currentMax: 6,
-            type: 'category'
+            type: 'category',
+            gridLineWidth: 0
         },
 
         yAxis: {
             currentMin: 0,
             currentMax: 10,
-            title: { text: 'Skill Level' }
+            title: { text: 'Skill Level' },
+            gridLineWidth: 0,
+            labels: []
+        }
+
+    };
+
+    $scope.beerChartConfig = {
+        options: {
+            //This is the Main Highcharts chart config. Any Highchart options are valid here.
+            //will be ovverriden by values specified below.
+            chart: {
+                type: 'line',
+                style: {
+                    backgroundColor: null
+                }
+            },
+            tooltip: {
+                style: {
+                    padding: 10,
+                    fontWeight: 'bold'
+                }
+            },
+            exporting: {
+                enabled: false
+            }
+        },
+
+        //Series object (optional) - a list of series using normal highcharts series options.
+        series: [{
+            data: [{y:7},{y:7.5},{y:9},{y:9.5},{y:8.5},{y:7},{y:4},{y:2},{y:0},{y:0},{y:0}],
+
+            showInLegend: false
+        }],
+
+        //Title configuration (optional)
+        title: {
+            text: null
+        },
+
+        //Boolean to control showng loading status on chart (optional)
+        loading: true,
+
+        //Configuration for the xAxis (optional). Currently only one x axis can be dynamically controlled.
+        //properties currentMin and currentMax provied 2-way binding to the chart's maximimum and minimum
+        xAxis: {
+            currentMin: 0,
+            currentMax: 10,
+            title: { text: 'Amount of Beer Consumed' },
+            gridLineWidth: 0
+        },
+
+        yAxis: {
+            currentMin: 0,
+            currentMax: 10,
+            title: { text: 'Productivity' },
+            gridLineWidth: 0,
+            labels: []
         }
 
     };
